b4784acd8d6de1e3904c08d0e8b3a5d1c131f32f
meohmiahh <mdiazaustin@gmail.com>
2021-11-24 10:44:09 -0600

    Added the search engine

--- app.js
+++ app.js
@@ -39,11 +39,23 @@ function displayTemperature(response) {
   iconElement.setAttribute
   ("src", 
   `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  iconElement.setAttribute("alt", respone.data.weather[0].description);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "9f2b26dba12b93ed4f31f1905e626c93";
-let city = "New York"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+    let apiKey = "9f2b26dba12b93ed4f31f1905e626c93";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
